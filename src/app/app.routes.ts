406a28ac1be511c91e16035759c22aed494eec4a
Miguel Moreau <miguelmoreau@gmail.com>
2022-07-09 09:51:34 -0500

    spa v1.0

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+
+import { RouterModule, Routes } from "@angular/router";
+
+const APP_ROUTES: Routes = [
+{ path: 'routePath', component: Component },
+{ path: '**' , pathMatch> 'full', redirectTo: '' }
+];
+
+export const app_routing = RouterModule.forRoot (APP_ROUTES)
